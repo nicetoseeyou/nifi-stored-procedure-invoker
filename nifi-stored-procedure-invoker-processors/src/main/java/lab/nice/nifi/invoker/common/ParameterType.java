bc840c31b37763713665ece0fd1b9df2fcd36fb9
nicetoseeyou <hello4world@yeah.net>
2019-09-19 04:08:57 +0800

    add build method

--- nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/common/ParameterType.java
+++ nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/common/ParameterType.java
@@ -1,5 +1,10 @@
 package lab.nice.nifi.invoker.common;
 
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Built-in stored procedure parameter type
+ */
 public enum ParameterType {
     IN(ParameterTypes.PROCEDURE_TYPE_IN),
     OUT(ParameterTypes.PROCEDURE_TYPE_OUT),
@@ -13,5 +18,21 @@ public enum ParameterType {
 
     public String getName() {
         return type;
+    }
+
+    /**
+     * Get parameter type from String type.
+     *
+     * @param type the type
+     * @return enum parameter type
+     */
+    public static ParameterType from(final String type) {
+        for (ParameterType parameterType : ParameterType.class.getEnumConstants()) {
+            if (StringUtils.equalsIgnoreCase(parameterType.type, type)) {
+                return parameterType;
+            }
+        }
+        throw new IllegalArgumentException("Type:" + type + " is not a valid "
+                + "lab.nice.nifi.invoker.common.ParameterType.java value.");
     }
 }
