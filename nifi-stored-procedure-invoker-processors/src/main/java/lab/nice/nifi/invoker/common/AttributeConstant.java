0b6b41a1432a4e9701f86b0971b86771710127be
nicetoseeyou <hello4world@yeah.net>
2019-09-19 04:11:05 +0800

    remove redundant constant

--- nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/common/AttributeConstant.java
+++ nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/common/AttributeConstant.java
@@ -3,16 +3,12 @@ package lab.nice.nifi.invoker.common;
 import java.util.regex.Pattern;
 
 public final class AttributeConstant {
-
-    public static final String PROCEDURE_TYPE_IN = "in";
-    public static final String PROCEDURE_TYPE_OUT = "out";
-    public static final String PROCEDURE_TYPE_INOUT = "inout";
-
     public static final String PROCEDURE_VALUE_TEMPLATE = "procedure.args.%s.%d.value";
     public static final String PROCEDURE_FORMAT_TEMPLATE = "procedure.args.%s.%d.format";
     public static final String PROCEDURE_NAME_TEMPLATE = "procedure.args.%s.%d.name";
 
-    public static final Pattern PROCEDURE_TYPE_ATTRIBUTE = Pattern.compile("procedure\\.args\\.(in|out|inout)\\.(\\d+)\\.type");
+    public static final Pattern PROCEDURE_TYPE_ATTRIBUTE =
+            Pattern.compile("procedure\\.args\\.(in|out|inout)\\.(\\d+)\\.type");
 
     private AttributeConstant() {
 
