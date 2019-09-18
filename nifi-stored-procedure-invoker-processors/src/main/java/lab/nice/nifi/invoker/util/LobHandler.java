0e2df2debc20913d81dbc4f635cb4a5d42d6c36d
nicetoseeyou <hello4world@yeah.net>
2019-09-19 03:57:25 +0800

    add comments

--- nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/util/LobHandler.java
+++ nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/util/LobHandler.java
@@ -10,6 +10,10 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Auto free CLOB/NCLOB handler.
+ * Support to construct CLOB/NCLOB from character stream, and free CLOB/NCLOB after commit.
+ */
 public class LobHandler implements AutoCloseable {
     private static final int DEFAULT_BUFFER_SIZE = 256;
 
@@ -17,12 +21,25 @@ public class LobHandler implements AutoCloseable {
     private final List<Clob> clobs;
     private final List<NClob> nClobs;
 
+    /**
+     * Construct handler with statement.
+     *
+     * @param statement the sql statement
+     */
     public LobHandler(final Statement statement) {
         this.statement = statement;
         this.clobs = new ArrayList<>();
         this.nClobs = new ArrayList<>();
     }
 
+    /**
+     * Create CLOB from character stream.
+     *
+     * @param reader the character stream to retrieve
+     * @return CLOB
+     * @throws SQLException if failed to create CLOB
+     * @throws IOException  if failed to read character stream
+     */
     public Clob clob(final Reader reader) throws SQLException, IOException {
         if (null == reader) {
             return null;
@@ -33,6 +50,14 @@ public class LobHandler implements AutoCloseable {
         }
     }
 
+    /**
+     * Create NCLOB from character stream.
+     *
+     * @param reader the character stream to retrieve
+     * @return NCLOB
+     * @throws SQLException if failed to create NCLOB
+     * @throws IOException  if failed to read character stream
+     */
     public NClob nClob(final Reader reader) throws SQLException, IOException {
         if (null == reader) {
             return null;
@@ -43,6 +68,14 @@ public class LobHandler implements AutoCloseable {
         }
     }
 
+    /**
+     * Write character to CLOB from character stream.
+     *
+     * @param clob   the target CLOB
+     * @param reader the character stream
+     * @throws IOException  if failed to read character stream
+     * @throws SQLException if failed to write character to CLOB
+     */
     private void writeToClob(final Clob clob, final Reader reader) throws IOException, SQLException {
         final CharBuffer charBuffer = CharBuffer.allocate(DEFAULT_BUFFER_SIZE);
         long position = 1;
@@ -54,6 +87,12 @@ public class LobHandler implements AutoCloseable {
         }
     }
 
+    /**
+     * Auto close (free) CLOB/NCLOB
+     *
+     * @throws SQLException if failed to free CLOB/NCLOB
+     * @see AutoCloseable#close()
+     */
     @Override
     public void close() throws SQLException {
         for (Clob clob : clobs) {
