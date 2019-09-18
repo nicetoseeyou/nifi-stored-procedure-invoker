7bc632a78483873cd148292c2781b549f50e2391
nicetoseeyou <hello4world@yeah.net>
2019-09-18 21:29:08 +0800

    complete attribute to stored procedure parameter

--- nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/util/AttributeHandler.java
+++ nifi-stored-procedure-invoker-processors/src/main/java/lab/nice/nifi/invoker/util/AttributeHandler.java
@@ -3,6 +3,8 @@ package lab.nice.nifi.invoker.util;
 
 import lab.nice.nifi.invoker.common.AttributeConstant;
 import lab.nice.nifi.invoker.common.Parameter;
+import lab.nice.nifi.invoker.common.ParameterType;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -14,7 +16,7 @@ public final class AttributeHandler {
     /**
      * Retrieve stored procedure parameter from NiFi processor properties or NiFi FlowFile attributes.
      *
-     * @param attributes          NiFi processor properties or NiFi FlowFile attributes
+     * @param attributes NiFi processor properties or NiFi FlowFile attributes
      * @param parameters the target stored procedure parameter map
      */
     public static void retrieveProcedureParameter(final Map<String, String> attributes,
@@ -26,20 +28,68 @@ public final class AttributeHandler {
                 final String parameterType = matcher.group(1);
                 final Integer parameterIndex = Integer.parseInt(matcher.group(2));
                 final Integer jdbcType = Integer.parseInt(entry.getValue());
+
                 final Parameter parameter =
                         parameters.getOrDefault(parameterIndex, new Parameter(parameterType, parameterIndex, jdbcType));
-                if (AttributeConstant.PROCEDURE_TYPE_IN.equals(parameterType)){
 
-                }else if (AttributeConstant.PROCEDURE_TYPE_OUT.equals(parameterType)){
+                final String valueAttribute = valueAttribute(parameterType, parameterIndex);
+                final String formatAttribute = formatAttribute(parameterType, parameterIndex);
+                final String nameAttribute = nameAttribute(parameterType, parameterIndex);
 
-                }else if (AttributeConstant.PROCEDURE_TYPE_INOUT.equals(parameterType)){
-
-                }else {
+                if (AttributeConstant.PROCEDURE_TYPE_IN.equals(parameterType)) {
+                    inParameter(parameter, attributes.get(valueAttribute), attributes.get(formatAttribute));
+                    parameter.setType(ParameterType.IN);
+                } else if (AttributeConstant.PROCEDURE_TYPE_OUT.equals(parameterType)) {
+                    outParameter(parameter, attributes.get(nameAttribute));
+                    parameter.setType(ParameterType.OUT);
+                } else if (AttributeConstant.PROCEDURE_TYPE_INOUT.equals(parameterType)) {
+                    inParameter(parameter, attributes.get(valueAttribute), attributes.get(formatAttribute));
+                    outParameter(parameter, attributes.get(nameAttribute));
+                    parameter.setType(ParameterType.INOUT);
+                } else {
                     //do nothing, should never be
                 }
             }
         }
     }
 
+    /**
+     * Assign value and value format to parameter if the given value and format not NULL value.
+     *
+     * @param parameter the target parameter
+     * @param value     the given input value
+     * @param format    the given input value format
+     */
+    private static void inParameter(final Parameter parameter, final String value, final String format) {
+        if (null != value) {
+            parameter.setValue(value);
+        }
+        if (null != format) {
+            parameter.setFormat(format);
+        }
+    }
 
+    /**
+     * Assign name for parameter if given name is not blank
+     *
+     * @param parameter the target parameter
+     * @param name      the given name
+     */
+    private static void outParameter(final Parameter parameter, final String name) {
+        if (StringUtils.isNotBlank(name)) {
+            parameter.setName(name);
+        }
+    }
+
+    private static String valueAttribute(final String parameterType, final Integer parameterIndex) {
+        return String.format(AttributeConstant.PROCEDURE_VALUE_TEMPLATE, parameterType, parameterIndex);
+    }
+
+    private static String formatAttribute(final String parameterType, final Integer parameterIndex) {
+        return String.format(AttributeConstant.PROCEDURE_FORMAT_TEMPLATE, parameterType, parameterIndex);
+    }
+
+    private static String nameAttribute(final String parameterType, final Integer parameterIndex) {
+        return String.format(AttributeConstant.PROCEDURE_NAME_TEMPLATE, parameterType, parameterIndex);
+    }
 }
